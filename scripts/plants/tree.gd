e59dc860275d80f001f9d50d5b326f97a0da74b4
MrTurtle996 <49366964+MrTurtle996@users.noreply.github.com>
2022-04-29 23:56:57 +0200

    placeable seed trunks

--- scripts/plants/tree.gd
+++ scripts/plants/tree.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var tilemap
+var seed_tile
+var trunks = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tilemap = self.get_parent().get_node("TileMap")
+	seed_tile = tilemap.world_to_map(self.global_position)
+	trunks.append(seed_tile)
+	
+func _process(delta):
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _unhandled_input(event):
+	if(event.is_action_pressed("mouse_button_left")):
+		var clicked_tile = tilemap.world_to_map(get_viewport().get_mouse_position())
+		
+		if(tilemap.get_cellv(clicked_tile) != 2): #only possible if tile is not stone
+			if(clicked_tile.y < seed_tile.y): #over seed can only be trunk
+				var trunk = trunks[trunks.size() - 1]
+				if(clicked_tile.y == trunk.y - 1 and (clicked_tile.x >= (trunk.x - 1) and clicked_tile.x <= (trunk.x + 1))):
+					tilemap.set_cellv(clicked_tile, 1) #place trunk tile
+					trunks.append(clicked_tile)
+		
+		
