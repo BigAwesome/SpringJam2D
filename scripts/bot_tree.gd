43acc1d88ea2ed8a235276d40a914113e22dc020
F <12214194+BigAwesome@users.noreply.github.com>
2022-04-30 17:35:43 +0200

    Added bot score and basic decissions

---
+++ scripts/bot_tree.gd
@@ -0,0 +1,154 @@
+extends Node2D
+
+export(bool) var randomPosition = true
+export(Array, Vector2) var spawnArea = [Vector2(),Vector2()] #start tile of the area, end tile of the area
+export(Vector2) var spawnPoint
+
+var map
+var levelmap
+var treemap
+var seed_tile
+var trunks = []
+var roots = []
+var tick_delta = Score.tick
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score = Score.Points.new()
+	map = self.get_parent()
+	levelmap = self.get_parent().get_node("LevelMap")
+	treemap = self.get_node("TreeMap")
+	
+	_calculate_spawn_point()
+	self.global_position = spawnPoint
+	treemap.global_position = Vector2(0, 0)
+	
+	seed_tile = levelmap.world_to_map(self.global_position)
+	trunks.append(seed_tile)
+
+func _process(delta):
+	if(tick_delta >= Score.tick):
+		tick_delta = 0
+		_bot_process()
+	else:
+		tick_delta += delta
+
+func _calculate_spawn_point():
+	if(randomPosition):
+		var randtest = rand_range(spawnArea[0].x, spawnArea[1].x)
+		spawnPoint.x = 64 * int(rand_range(spawnArea[0].x, spawnArea[1].x)) + 32
+		spawnPoint.y = 64 * int(rand_range(spawnArea[0].y, spawnArea[1].y)) + 32
+	else:
+		spawnPoint.x = 64 * spawnPoint.x + 32
+		spawnPoint.y = 64 * spawnPoint.y + 32
+
+
+func _place_tile(tile_pos, tile):
+	treemap.set_cellv(tile_pos, tile)
+
+func _bot_process():
+	print(score.get_power())
+	if(score.get_power() >= 5):
+		#UP
+		print("up")
+	elif(score.get_power() < 5):
+		#place more roots
+		print("down")
+		
+	else:
+		#IM F****ED
+		print("ded")
+		
+	
+
+func _build_on_seed():
+	var clicked_tile = levelmap.world_to_map(get_global_mouse_position())
+		
+	if(clicked_tile.x < map.width && clicked_tile.y < map.height):
+		if(levelmap.get_cellv(clicked_tile) != 0): #only possible if tile is not stone ------------------------- set to acctual tile number later on
+			if(clicked_tile.y < seed_tile.y): #over seed can only be trunk
+				
+				_build_trunk(clicked_tile)
+				
+			else: #under seed can only be root
+				
+				_build_root(clicked_tile)
+				
+		else:
+			print("Cannot grow on stone")
+	else:
+		print("Out of map area")
+	
+
+func _build_trunk(build_pos):
+	
+	if(treemap.get_cellv(build_pos) != Score.get_tile("trunks")):
+		if(_check_connection_to_seed(build_pos, "trunk")):
+			_place_tile(build_pos, Score.get_tile("trunks")) #place trunk tile
+			trunks.append(build_pos)
+			_add_score(build_pos, "trunks")
+		else:
+			print("Trunk needs to be connected to seed")
+	else:
+		print("Already existing trunk at this position")
+
+func _build_root(build_pos):
+	if(treemap.get_cellv(build_pos) != Score.get_tile("roots")):
+		if(_check_connection_to_seed(build_pos, "root")):
+			_place_tile(build_pos, Score.get_tile("roots")) # place root tile
+			roots.append(build_pos)
+			
+			_add_score(build_pos, "roots")
+			
+		else:
+			print("Root needs to be connected to seed")
+	else:
+		print("Already existing root at this position")
+
+
+func _check_connection_to_seed(build_pos, tile):
+	if(tile == "trunk"):
+		var trunk = trunks[trunks.size() - 1]
+		return (build_pos.y == trunk.y - 1 and (build_pos.x >= (trunk.x - 1) and build_pos.x <= (trunk.x + 1)))
+	else:
+		#area in which needs to be at least one root
+		var start = Vector2(build_pos.x - 1, build_pos.y - 1) 	#current roots surrounding: #  #  #
+		var end = Vector2(build_pos.x + 1, build_pos.y - 1)		# "#" is root area			O  x  O
+		var index = start										# "O" is empty area			O  O  O
+	
+		while index.y <= end.y:
+			var cell = treemap.get_cellv(index)
+			if(cell == Score.get_tile("roots") or index == seed_tile): #if root tile is around
+				
+				if(index != start && index != end):
+					return true
+					break
+				else:
+					
+					if(_check_no_diagonal_stones(index, start)):
+						return true
+						break
+			
+			if(index.x < end.x):
+				index.x = index.x + 1
+			else:
+				index.y = index.y + 1
+				index.x = build_pos.x - 1
+				
+		return false
+
+func _check_no_diagonal_stones(tile_to_check, top_left_pos):
+	var left_or_right = - 1
+	if(tile_to_check == top_left_pos): left_or_right = 1
+	var no_stone_below = levelmap.get_cellv(Vector2(tile_to_check.x, tile_to_check.y + 1)) != 0
+	var no_stone_beside = levelmap.get_cellv(Vector2(tile_to_check.x + (left_or_right), tile_to_check.y)) != 0
+	
+	return (no_stone_below or no_stone_beside)
+
+func _add_score(tile_pos, build_tile_name):
+	var value = levelmap.get_cellv(tile_pos)
+	Score.build_tile(build_tile_name)
+	Score.build_id(value)
+	if(value == Score.get_tile("water")):
+		Score.build_tile("trunks")
--- scripts/score.gd
+++ scripts/score.gd
@@ -4,17 +4,20 @@ extends Node2D
 # Declare member variables here. Examples:
 onready var plants = []
 var ui_node
+var map
 export var tick = 2
 var tick_delta = tick
+var _player_points
 
 export var tiles ={"rock": 0, "dirt": 1, "water": 5, "air": -1, "leaf": -1, "trunks": 3, "roots": 4}
 
 export var resource_value = [0, 0, 0, -4, -1, 8, -1]
 
-var owned = [0, 0, 0, 0, 0, 0, 0] # amount of owned tiles
-var score = [0, 0, 0, 0, 0, 0, 0] # amount of points (over time)
-var power = 10
 
+
+var bot = preload("res://nodes/plants/bot_tree.tscn")
+export var spawn_time = 5
+var spawn_time_delta = 0
 
 
 func get_tile(name):
@@ -24,26 +27,56 @@ func get_value(name):
 	return resource_value[get_tile(name)]
 
 func build_id(id):
-	owned[id] += 1
+	_player_points.build_id(id)
 func build_tile(name):
-	owned[get_tile(name)] += 1
+	_player_points.build_tile(name)
 
 func _tick_update():
-	for i in len(owned):
-		score[i] += owned[i] * resource_value[i]
-		power += owned[i] * resource_value[i]
-
+	_player_points._tick_update()
+	spawn_time_delta += 1
+	
+	if(spawn_time_delta == spawn_time):
+		var node = bot.instance()
+		node.spawnArea = [Vector2(), Vector2(map.height,map.width)]
+		map.add_child(node)
+		node.set_owner(map)
+		spawn_time_delta = 0
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_player_points = Points.new()
 	plants = get_tree().get_nodes_in_group("Plant")
 	ui_node = get_tree().get_nodes_in_group("Score")[0]
+	map = get_tree().get_nodes_in_group("Map")[0]
 	#set_resource("roots", score[get_tile("roots")])
 	#set_resource("trunks", score[get_tile("trunks")])
 
 func _process(delta):
-	ui_node.get_node("Power").text = "Power: " + power as String
+	ui_node.get_node("Power").text = "Power: " + _player_points.get_power() as String
 	if(tick_delta >= tick):
 		tick_delta = 0
 		_tick_update()
 	else:
 		tick_delta += delta
+
+class Points:
+	var _owned = [0, 0, 0, 0, 0, 0, 0] # amount of owned tiles
+	var _score = [0, 0, 0, 0, 0, 0, 0] # amount of points (over time)
+	var _power = 10
+	func _init():
+		pass
+		
+	func get_tile(name):
+		return Score.tiles[name]
+	func get_value(name):
+		return Score.resource_value[get_tile(name)]
+	func build_id(id):
+		_owned[id] += 1
+	func build_tile(name):
+		_owned[get_tile(name)] += 1
+	func get_power():
+		return _power
+	func _tick_update():
+		for i in len(_owned):
+			_score[i] += _owned[i] * Score.resource_value[i]
+			_power += _owned[i] * Score.resource_value[i]
